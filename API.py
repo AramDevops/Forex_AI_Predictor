24de6a1361618056e3498cdfbc0a7be63016971a
Aram <79871730+AramDevops@users.noreply.github.com>
2023-04-17 12:57:38 -0400

    Update API.py

--- API.py
+++ API.py
@@ -10,12 +10,14 @@ DECIMALS = str(6)
 @lru_cache(1000)
 def request_time_series_conversion(base: str, amount: float, start_date: str, end_date: str) -> DataFrame:
     symbols = SIGNS_verification_str(base)
-    """url = f'https://api.exchangerate.host/timeseries?start_date={start_date}&end_date={end_date}' \
-          f'&base='+base+'&amount='+str(amount) +'&places='+DECIMALS+'&symbols='+ symbols"""
-    url = f'https://freecurrencyapi.net/api/v2/historical?apikey=2f059550-4442-11ec-9edd-a55e2712eb4c&base_currency={base}&date_from={start_date}&date_to={end_date}'
+    url = f'https://api.exchangerate.host/timeseries?start_date={start_date}&end_date={end_date}' \
+          f'&base='+base+'&amount='+str(amount) +'&places='+DECIMALS+'&symbols='+ symbols
+    """url = f'https://freecurrencyapi.net/api/v2/historical?apikey=2f059550-4442-11ec-9edd-a55e2712eb4c&base_currency={base}&date_from={start_date}&date_to={end_date}'"""
     response = requests.get(url)
-    time_series_conversion_dict = api_formatter(response.json(), ['base_currency', 'data'])
-    df = pd.DataFrame(time_series_conversion_dict['data']).T
+    print(url)
+    print(response)
+    time_series_conversion_dict = api_formatter(response.json(), ['base', 'rates'])
+    df = pd.DataFrame(time_series_conversion_dict['rates']).T
     df = df.round(5)
     return df
 
